d022eed6c4d24e7115586511ba13f1837e5e394f
taka-hoop <takanari_gal1318@ezweb.ne.jp>
2019-11-05 17:24:21 +0900

    自動更新実装

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,18 +1,20 @@
 $(function(){
   function buildPost(post){
-    img = post.image ? `<img src='${post.image}'>` : "";
-    var html = `<div class="wrapper__chat-main__messages__message">
+    img = post.image.url? `<img src='${post.image.url}'>` : "";
+    var html = `<div class="wrapper__chat-main__messages__message" data-message-id="${post.id}">
                   <div class="wrapper__chat-main__messages__message__upper-info">
                     <p class="wrapper__chat-main__messages__message__upper-info__talker">
                     ${post.user}
                     </p>
                     <p class="wrapper__chat-main__messages__message__upper-info__date">
                     ${post.updated_at}
-                    </p><p class="wrapper__chat-main__messages__message__text">
+                    </p>
+                    <p class="wrapper__chat-main__messages__message__text">
                     ${post.content}
                     </p>
-                  <p></p>
-                  ${img}
+                    <p>
+                    ${img}
+                    </p>
                   </div>
                 </div>`
     return html;
@@ -35,13 +37,36 @@ $(function(){
       $(".wrapper__chat-main__messages").append(html);
       $(".wrapper__chat-main__messages").animate({scrollTop: $(".wrapper__chat-main__messages")[0].scrollHeight}, "fasts");
       $(".wrapper__chat-main__form__new__message__input-box__submit__btn").prop("disabled", false);
-      $(message_content).val("")
+      $("#message_content").val("")
+
     })
     .fail(function(){
       alert("エラー");
     })
-  })
-});
+  });
 
-// 2019/11/2 doneの中にanimateメソッド直接入れてる
-// 　　　　　　appendのイメージ部分が何かおかしくてエラーが出てる？
+  var reloadMessages = function () {
+    if (window.location.href.match(/\/groups\/\d+\/messages/)){
+      var last_message_id = $('.wrapper__chat-main__messages__message:last').data("message-id");
+
+      $.ajax({
+        url: "api/messages",
+        type: 'get',
+        dataType: 'json',
+        data: {id: last_message_id}
+      })
+      .done(function (messages) {
+        var insertHTML = '';
+        messages.forEach(function (message) {
+          insertHTML = buildPost(message);
+          $('.wrapper__chat-main__messages').append(insertHTML);
+        })
+        $('.wrapper__chat-main__messages').animate({scrollTop: $('.wrapper__chat-main__messages')[0].scrollHeight}, 'fasts');
+      })
+      .fail(function () {
+        alert('自動更新に失敗しました');
+      });
+    }
+  };
+  setInterval(reloadMessages, 5000);
+  });
